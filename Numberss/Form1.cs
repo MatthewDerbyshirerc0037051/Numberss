f35dfab8c19b02a11d7601df49487a00e396e62f
Matthew Derbyshire <rc0037051@student.riversidecollege.ac.uk>
2020-02-11 12:28:30 +0000

    6uu5u75u75

--- Numberss/Form1.cs
+++ Numberss/Form1.cs
@@ -33,8 +33,42 @@ namespace Numberss
             double c = 299792458;
             richTextBox1.AppendText(c.ToString("") + "\n");
             richTextBox1.AppendText(c.ToString("N0") + "\n");
-            richTextBox1.AppendText(c.ToString("0.0000000000000000E0000000000000000") + "\n");
-            richTextBox1.AppendText(c.ToString("0.000E000") + "\n");
+            richTextBox1.AppendText(c.ToString("0.000000000000000E00") + "\n");
+            richTextBox1.AppendText(c.ToString("0.000E00") + "\n");
+
+            string st = "three";
+            try
+            {
+                int it = int.Parse(st);
+            }
+            catch
+            {
+                MessageBox.Show(st + " is not an integer");
+            }
+
+            string s2 = "3.2";
+            try
+            {
+                int i2 = int.Parse(s2);
+            }
+            catch
+            {
+                MessageBox.Show(s2 + " is not an integer");
+                int i2 = 0;
+            }
+
+            string s3 = "pi";
+            try
+            {
+                double d2 = double.Parse(s3);
+            }
+            catch
+            {
+                double d2 = 0;
+            }
+
+
+
         }
 
         private void TextBox1_TextChanged(object sender, EventArgs e)
